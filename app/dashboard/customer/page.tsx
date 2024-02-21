c7e9abeb3ef5b1e397b88d5b297edef0e5b715e8
Ivan Romero Gutierrez <manny.parte.2@gmail.com>
2024-02-21 11:40:16 -0400

    4to

---
+++ app/dashboard/customer/page.tsx
@@ -0,0 +1,9 @@
+export default function page()
+{
+    return <div>          
+            Customer Page
+                <p>
+                    Esa pagina esta pensado para el customer
+                </p>
+            </div>
+}
